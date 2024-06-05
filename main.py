21650b218ec3cecdd9280acb93748e6119677ed0
Nícolas Michielon <61835002+nicolasmichielon@users.noreply.github.com>
2024-06-05 20:03:21 -0300

    Added verificarGanhador()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-class Gomoku:
+class Gomoku():
     def __init__(self, tamanho=19):
         self.tamanho = tamanho
         self.tabuleiro = []
@@ -20,13 +20,70 @@ class Gomoku:
       col -= 1
       if self.tabuleiro[row][col] == "-":
         self.tabuleiro[row][col] = x
-      else:
-          print("Posição já ocupada.")
-
-
-
-
+        return True
+      return False
+    
+    def verificarGanhador(self):
+        #Horizontal check
+        p0 = 0
+        p1 = 0
+        for row in self.tabuleiro:
+            p0 = 0
+            p1 = 0
+            for i in row:
+                if i == "X":
+                    p0 += 1
+                if i == "O":
+                    p1 += 1
+            if p0 == 5:
+                return 0
+            elif p1 == 5:
+                return 1
+        #Vertical Check
+        c = 0
+        r = 0
+        p0 = 0
+        p1 = 0
+        aux = 0
+        while c < len(self.tabuleiro):
+            r = 0
+            while r < len(self.tabuleiro):
+                if self.tabuleiro[r][c] == "X":
+                    p0 += 1
+                    p1 = 0
+                    if p0 == 5:
+                        return 0
+                elif self.tabuleiro[r][c] == "O":
+                    p0 = 0
+                    p1 += 1
+                    if p1 == 5:
+                        return 1
+                else:
+                    p0 = 0
+                    p1 = 0
+                r += 1
+            c += 1
+        return "Continua"
+    #Diagonal Check
+        
+                
+    
+class Game():
+    def __init__(self, tabuleiro):
+        self.tabuleiro = tabuleiro
+        self.players = ["X", "O"]
+        self.currentPlayer = 0
 
 gomoku = Gomoku()
 gomoku.posicionar(3,4, "X")
+gomoku.posicionar(3,5, "O")
+gomoku.posicionar(3,6, "O")
+gomoku.posicionar(3,7, "X")
+gomoku.posicionar(3,8, "O")
+gomoku.posicionar(4,8, "O")
+gomoku.posicionar(5,8, "O")
+gomoku.posicionar(6,8, "O")
+gomoku.posicionar(7,8, "O")
+gomoku.posicionar(3,9, "O")
 gomoku.print_tabuleiro()
+print(gomoku.verificarGanhador())
